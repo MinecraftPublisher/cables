8026662fe6ddcd52e7b3b8137c486305d21a4894
martia_f <mforoud86@gmail.com>
2021-10-22 17:05:06 +0330

    Added a brand new require.js app file.

---
+++ require.js
@@ -0,0 +1,20 @@
+if (window) {
+    global.require = (name) => {
+        fetch('https://www.npmjs.com/package/' + name).then(response => {
+            if (response.status === 404) {
+                let dir_path = window.location.href.split('/').slice(0, window.location.href.split('/').length - 1).join('/') + '/'
+                fetch(dir_path + name).then(response => {
+                    if (response.status === 404) {
+                        throw new ReferenceError('Error: Unable to find module.')
+                    } else {
+                        response.text().then(data => {
+                            import('data:text/javascript;charset=utf-8;base64,' + btoa(data))
+                        })
+                    }
+                })
+            } else {
+                import('https://unpkg.com/' + name)
+            }
+        })
+    }
+}
