e30ae45574cb67917f7276c256d7252439017e52
martia_f <mforoud86@gmail.com>
2021-10-23 11:21:52 +0330

    Added a fallback for non-root users

--- bin/index.js
+++ bin/index.js
@@ -5,35 +5,35 @@
  * The All-In-One package manager for your CLI needs and deeds.
  */
 
+let version = '3.5.0'
 let isWin = process.platform === 'win32'
 if (isWin) {
-    console.clear()
     console.log(chalk.redBright.bold('Error: We still haven\'t figured out a way to create symlinks on Windows, Sorry about that.'))
     process.exit()
+}
+function isCurrentUserRoot() {
+    return process.getuid() == 0;
 }
 
 const fetch = require('node-fetch')
 const chalk = require('chalk')
 const shell = require('shelljs')
 const fs = require('fs')
-
-let version = '3.4.0'
 let filepath = module.filename
 let path = '/usr/local/cables/'
+if (!isCurrentUserRoot()) { path = '/' + filepath.substring(1, filepath.length - 'bin/index.js'.length); console.log(chalk.redBright.bold('Error: Cables needs to be ran as sudo, However we have a fallback. We\'ve got you. Next time, Use cables as sudo.')) }
 let args = process.argv.slice(2)
 if (!globalThis.fetch) { globalThis.fetch = fetch; }
-shell.mkdir(path)
-shell.mkdir(path + 'cables_files')
+
 const tools = {
     'help': () => {
-        console.clear()
         console.log(
             chalk.green.bold('Cables Package Manager v' + version + '\n') +
             chalk.yellowBright('help') + chalk.blueBright('                                 Show this menu\n') +
             chalk.yellowBright('update') + chalk.blueBright('                               Updates cables to the latest version\n') +
             chalk.yellowBright('install <PACKAGE_NAME_OR_URL>') + chalk.blueBright('        Install a package\n') +
             chalk.yellowBright('remove <PACKAGE_NAME_OR_URL>') + chalk.blueBright('         Uninstall a package\n') +
-            chalk.yellowBright('clean') + chalk.blueBright.bold('                                 This command removes ALL of your existing packages and unlinks them, Be careful.\n') + 
+            chalk.yellowBright('clean') + chalk.blueBright.bold('                                 This command removes ALL of your existing packages and unlinks them, Be careful.\n') +
             chalk.greenBright('Cables also creates symlinks of all installed packages in ') + chalk.yellowBright.bgBlueBright('/usr/local/bin/') + chalk.greenBright(' to prevent frustration.\n') +
             '\n\n'
         )
@@ -45,16 +45,15 @@ const tools = {
             fs.unlinkSync(file)
         })
         JSON.parse(fs.readFileSync(path + 'package.json').toString()).keys.forEach((package) => {
-            if(!(package === 'chalk' || package === 'shelljs')) {
+            if (!(package === 'chalk' || package === 'shelljs')) {
                 fs.unlinkSync('/usr/local/bin/' + package)
-                shell.exec('npm uninstall ' + package)
+                shell.exec('sudo npm uninstall ' + package)
             }
         })
     },
     'update': () => {
         tools['patch']('cables')
         /*
-        console.clear()
         console.log(chalk.bold('Fetching data...'))
         fetch('https://raw.githubusercontent.com/MinecraftPublisher/cables/main/bin/index.js').then((response) => {
             console.log(chalk.bold('Converting data into plaintext...'))
@@ -71,7 +70,6 @@ const tools = {
                                 console.log(chalk.redBright.bold(err))
                             } else {
                                 fs.unlinkSync('/usr/local/bin/cables')
-                                console.clear()
                                 tools['update']()
                                 console.log(chalk.greenBright.bold('Cables has been updated to version ' + data.split('\n')[19].substring(15, data.split('\n')[19].length - 1) + '!'))
                             }
@@ -84,13 +82,13 @@ const tools = {
          */
     },
     'patch': (name_or_url) => {
-        if (/* name_or_url == 'cables' */ 2+2===5)
+        if (/* name_or_url == 'cables' */ 2 + 2 === 5)
             tools['update']()
         else {
             fetch('https://npmjs.com/package/' + name_or_url).then((response) => {
                 if (response.status !== 404) {
                     console.log(chalk.greenBright('Found an NPM package on the registry! Starting NPM cloning...'))
-                    shell.exec('cd ' + path + 'cables_files/ && npm i ' + name_or_url, function (code, stdout, stderr) {
+                    shell.exec('cd /usr/local/cables && sudo npm i ' + name_or_url, function (code, stdout, stderr) {
                         console.log(chalk.bold('Successfully fetched package from NPM, Trying to patch files...'))
                         let patchPath = path + 'node_modules/' + name_or_url + '/'
                         let JSONfile = JSON.parse(fs.readFileSync(patchPath + 'package.json').toString() || '{}')
@@ -181,7 +179,7 @@ const tools = {
     'remove': (name) => {
         if (fs.existsSync('/usr/local/bin/' + name)) {
             console.log(chalk.bold('Removing package...'))
-            shell.exec('cd ' + path + 'cables_files && rm ' + name + '.js && npm uninstall ' + name, function (code, stdout, stderr) {
+            shell.exec('sudo cd ' + path + 'cables_files && sudo rm ' + name + '.js && sudo npm uninstall ' + name, function (code, stdout, stderr) {
                 console.log(chalk.bold('Removing symlink...'))
                 fs.unlinkSync('/usr/local/bin/' + name)
                 console.log(chalk.greenBright.bold('Package successfully removed!'))
@@ -198,7 +196,6 @@ if (args.length === 2) {
     } else if (args[0] == 'remove') {
         tools['remove'](args[1])
     } else {
-        console.clear()
         console.log(chalk.redBright.bold('Error: Unknown command! Try running the help command.'))
     }
 } else if (args.length === 1) {
@@ -209,15 +206,14 @@ if (args.length === 2) {
     } else if (args[0] == 'clean') {
         tools['clean']()
     } else if (args[0] == 'install') {
-        console.clear()
         console.log(chalk.redBright.bold('Error: Not enough arguments, Try running the help command.'))
     } else {
-        console.clear()
         console.log(chalk.redBright.bold('Error: Unknown command! Try running the help command.'))
     }
 } else {
     tools['help']()
 }
+
 
 module.exports = {
     'version': 'cables v' + version,
